24b778db70c9f1be2ee4aa9868223089302d272c
wolfram74 <phaugen@gmail.com>
2015-11-19 09:50:36 -0800

    display working! gorram negative axis

--- introduction/bare_template/js/walker.js
+++ introduction/bare_template/js/walker.js
@@ -6,5 +6,18 @@ function Walker(args){
 };
 
 Walker.prototype.display = function(){
+  var context = this.canvas[0].getContext('2d');
+  var yOff = this.canvas.height()/2
+  var xOff = this.canvas.width()/2
+  context.fillStyle = "rgba(0,0,0,1)"
+  context.fillRect(this.x+xOff, yOff-this.y, 1, 1)
+}
 
+var testRun = function(){
+  var bleh = new Walker({canvas: $('canvas').first()})
+  for(var i=0; i<100; i++){
+    console.log(i)
+    bleh.x = i
+    bleh.display()
+  }
 }
