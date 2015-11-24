6552359c5a67ad756cfc8d9829e1bb2f3303bd58
[01;31m[K"name"[m[K: "Stephanie Hutson <[01;31m[K"email"[m[K: null>
2015-11-23 17:07:25 -0800

    finish exerise 5

--- Chapter1/exercise1_5.py
+++ Chapter1/exercise1_5.py
@@ -9,31 +9,36 @@ class PVector:
     self.x += vector.x
     self.y += vector.y
 
+  def set_zero(self):
+    self.x = 0
+    self.y = 0
+
 class Runner:
   def __init__ (self):
     self.width = 5
     self.height = 5
     self.velocity = PVector(0, 0)
     self.location = PVector(50,100)
-    self.acceleration = PVector(1, 0)
+    self.acceleration = PVector(0, 0)
 
 
   def update(self):
     canvas.delete('all')
-    self.velocity = self.velocity.add(self.acceleration)
-    self.location = self.location.add(self.velocity)
+    self.velocity.add(self.acceleration)
+    if self.velocity.x < 0:
+      self.velocity.set_zero()
+    if self.velocity.y < 0:
+      self.velocity.set_zero()
+    self.location.add(self.velocity)
+    self.find_borders()
     canvas.create_oval(self.location.x, self.location.y, self.location.x + self.width, self.location.y+self.height)
     main.after(10, r.update)
 
   def accelerate(self):
-    self.accelerate.add(PVector(.005, 0))
+    self.acceleration.add(PVector(.01, 0))
 
   def brake(self):
-    self.accelerate.add(PVector(-.005, 0))
-    if self.velocity.x < 0:
-      self.velocity.x = 0
-    if self.velocity.y < 0:
-      self.velocity.y = 0
+    self.acceleration.add(PVector(-.01, 0))
 
   def find_borders(self):
     if self.location.x + self.width >= 800 or self.location.x < 0:
@@ -43,11 +48,22 @@ class Runner:
 
 
 main = Tk()
+
 canvas = Canvas(main, width=800, height=200)
 r = Runner()
+
+def leftKey(event):
+  r.brake()
+
+def rightKey(event):
+  r.accelerate()
+
+main.bind('<Left>', leftKey)
+main.bind('<Right>', rightKey)
+canvas.focus_set()
 canvas.pack()
-# main.after(10, r.update)
-# mainloop()
+main.after(10, r.update)
+mainloop()
 
 # a = PVector(0,0)
 # b = PVector(1,.005)
@@ -55,9 +71,9 @@ canvas.pack()
 # print a
 # print a.x
 # print a.y
-print r.acceleration
-print r.acceleration.x
-print r.acceleration.y
-print r.velocity.add(r.acceleration)
-print r.velocity
-print r.velocity.x
+# print r.acceleration
+# print r.acceleration.x
+# print r.acceleration.y
+# print r.velocity.add(r.acceleration)
+# print r.velocity
+# print r.velocity.x
